f0ae154f5484e147e0dfcb221ae2a0cb550a7261
TriSetianto123 <156288410+3nol04@users.noreply.github.com>
2025-11-08 21:48:18 +0700

    first commit

---
+++ app/Models/barang.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class barang extends Model
+{
+   use Hasfactory;
+
+   protected $guarded = ["id"];
+
+    //
+}
---
+++ app/Models/export_barang.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class export_barang extends Model
+{
+    //
+}
